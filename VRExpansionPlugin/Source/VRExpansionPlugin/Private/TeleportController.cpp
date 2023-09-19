1099b605fdd0bcd3f25ae74bbe933fd1097d42e0
Bobby <bobbylaudeman@gmail.com>
2023-09-19 16:37:34 -0700

    almost complete with Teleport controller conversion

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/TeleportController.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/TeleportController.cpp
@@ -3,6 +3,7 @@
 
 #include "TeleportController.h"
 #include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "NavigationSystem.h"
 
 ATeleportController::ATeleportController(const FObjectInitializer& ObjectInitializer)
@@ -125,52 +126,148 @@ void ATeleportController::UpdateArcSpline_Implementation(bool FoundValidLocation
         ArcSpline->AddSplinePoint(splinePoint, ESplineCoordinateSpace::Local, true);
     }
     ArcSpline->SetSplinePointType(SplinePoints.Num() - 1, ESplinePointType::CurveClamped, true);
-
+    int splinePointsLastIndex = ArcSpline->GetNumberOfSplinePoints() - 1;
     if (SplineMeshes.Num() < ArcSpline->GetNumberOfSplinePoints())
     {
-        PointDiffNum = (ArcSpline->GetNumberOfSplinePoints() - 1) - SplineMeshes.Num();
-        for (int i = 0; i <= PointDiffNum)
+        PointDiffNum = splinePointsLastIndex - SplineMeshes.Num();
+        for (int i = 0; i <= PointDiffNum; i++)
         {
-            
+            // Add new cylinder mesh
+           USplineMeshComponent* smc = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass()); 
+           // Make sure that scene is the right attachment object
+           smc->AttachToComponent(Scene, FAttachmentTransformRules::KeepRelativeTransform);
+           smc->SetGenerateOverlapEvents(false);
+           smc->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+           SplineMeshes.Add(smc);
+        }
+    }
+    for (int i = 0; i < SplineMeshes.Num(); i++)
+    {
+        if (i < splinePointsLastIndex)
+        {
+            SplineMeshes[i]->SetVisibility(true);
+            FVector startTangent, endTangent;
+            startTangent = ArcSpline->GetTangentAtSplinePoint(i, ESplineCoordinateSpace::Local);
+            endTangent = ArcSpline->GetTangentAtSplinePoint(i+1, ESplineCoordinateSpace::Local);
+            SplineMeshes[i]->SetStartAndEnd(SplinePoints[i], startTangent, SplinePoints[i+1], endTangent, true);
+        }
+        else
+        {
+            SplineMeshes[i]->SetVisibility(false);
         }
     }
 }
 
 void ATeleportController::UpdateArcEndpoint_Implementation(FVector NewLocation, bool ValidLocationFound)
 {
-
+    ArcEndPoint->SetVisibility(ValidLocationFound && IsTeleporterActive);
+    ArcEndPoint->SetWorldLocation(NewLocation, false, nullptr, ETeleportType::TeleportPhysics);
+    Arrow->SetWorldRotation(TeleportRotation + TeleportBaseRotation);
 }
 
 void ATeleportController::GetTeleportDestination_Implementation(bool RelativeToHMD, FVector &Location, FRotator &Rotation)
 {
+    FVector devicePosition;
+    FQuat deviceRotation;
+    GEngine->XRSystem->GetCurrentPose(IXRTrackingSystem::HMDDeviceId, deviceRotation, devicePosition);
+    if (RelativeToHMD)
+    {
+        FVector heightAgnosticPosition = FVector(devicePosition.X, devicePosition.Y, 0.0f);
+        Location = LastValidTeleportLocation - TeleportRotation.RotateVector(heightAgnosticPosition);
+        Rotation = TeleportRotation;
+    }
+    else
+    {
+        Location = LastValidTeleportLocation;
+        Rotation = TeleportRotation;
+    }
 }
 
 void ATeleportController::GetTeleWorldLocAndForwardVector_Implementation(FVector &WorldLoc, FVector &ForwardVector)
 {
+    WorldLoc = OwningMotionController->GetComponentLocation();
+    ForwardVector = UKismetMathLibrary::GetForwardVector(RotOffset + OwningMotionController->GetComponentRotation());
 }
 
 void ATeleportController::IfOverWidget_Use_Implementation(bool bPressed, bool &WasOverWidget)
 {
+    if (IsLaserBeamActive)
+    {
+        if (WidgetInteraction->IsOverInteractableWidget() || WidgetInteraction->IsOverFocusableWidget())
+        {
+            if (bPressed)
+            {
+                WidgetInteraction->PressPointerKey(EKeys::LeftMouseButton);
+            }
+            else
+            {
+                WidgetInteraction->ReleasePointerKey(EKeys::LeftMouseButton);
+            }
+            WasOverWidget = true;
+            return;
+        }
+    }
+    WasOverWidget = false;
 }
 
 void ATeleportController::InitController_Implementation()
 {
+    if (bIsLocal)
+    {
+        EControllerHand hand;
+        OwningMotionController->GetHandType(hand);
+        switch (hand)
+        {
+        case EControllerHand::Left:
+            WidgetInteraction->VirtualUserIndex = 0;
+            WidgetInteraction->PointerIndex = 0;
+            break;
+        
+        case EControllerHand::Right:
+            WidgetInteraction->VirtualUserIndex = 0;
+            WidgetInteraction->PointerIndex = 1;
+            break;
+        
+        default:
+            break;
+        }
+    }
 }
 
 void ATeleportController::ToggleTick_Implementation()
 {
+    SetActorTickEnabled(IsTeleporterActive || IsLaserBeamActive || ActorBeingThrown->IsValidLowLevel());
 }
 
 void ATeleportController::ClearLaserBeam_Implementation()
 {
+    for (USplineMeshComponent* mesh : LaserSplineMeshes)
+    {
+        mesh->DestroyComponent();
+    }
+    LaserSplineMeshes.Empty();
+    LaserSpline->ClearSplinePoints(true);
 }
 
 void ATeleportController::CreateLaserSpline_Implementation()
 {
+    if (bUseSmoothLaser)
+    {
+        for (int i = 0; i < NumberOfLaserSplinePoints - 1; i++)
+        {
+            // Might need to specify the actual spline mesh to use, maybe a variable that is set in the blueprint?
+           USplineMeshComponent* smc = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass()); 
+           smc->AttachToComponent(Scene, FAttachmentTransformRules::KeepRelativeTransform);
+           smc->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+           smc->SetGenerateOverlapEvents(false);
+           LaserSplineMeshes.Add(smc);
+        }
+    }
 }
 
 void ATeleportController::FilterGrabspline_Implementation(UPARAM(ref) TArray<FVector> &Locations, UPARAM(ref) FVector &Target, int32 ClosestIndex, double ClosestDist)
 {
+    
 }
 
 void ATeleportController::UpdateLaserBeam_Implementation(double Deltatime, FVector SmoothedLoc, EDrawDebugTrace::Type DrawType)
